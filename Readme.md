f54f03de9270c1e0d32aa901f304096173d8fb62
avishekBS <a.b.sribasbanikam@gmail.com>
2022-02-07 21:26:18 +0530

    different datatypes

--- Readme.md
+++ Readme.md
@@ -1,1 +1,8 @@
 ###CREATE BASIC PROJECT on PROTOBUFFER
+
+**Datatype conversion in java:
+1) int32, int64, float, double   --> int, long float, double
+2) bool --> boolean
+3) enum --> enum
+4) Lists --> java.util.list
+5) Map --> java.util.Map
--- protoBuffExample/src/main/java/com/codelate/service/EmployeeWriter.java
+++ protoBuffExample/src/main/java/com/codelate/service/EmployeeWriter.java
@@ -2,17 +2,30 @@ package com.codelate.service;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.codelate.model.protos.Employee;
+import com.codelate.model.protos.Employee.DEPARTMENT;
+import com.codelate.model.protos.EmployeeWrapper;
+import com.codelate.model.protos.ProjectDetails;
 
 public class EmployeeWriter {
 	public static void main(String[] args) throws IOException {
+
 		Employee employee = Employee.newBuilder()
 				.setId(11111)
 				.setName("Avishek")
 				.setDesignation("Developer")
+				.setProjectId(345678L)
+				.setFixedSalary(54678.69f)
+				.setVariableSalary(5678.98)
+				.setDepartment(DEPARTMENT.IT)
+				.addAllSkills(getSkills())
+				.putAllAssessmentScore(getAssessmentScores())
+				.setProjectdetails(getProjectDetails())
 				.build();
-
 		String outputFilename = "serialize_Employee";
 		System.out.println("saving Employee to file:" + outputFilename);
 
@@ -21,5 +34,27 @@ public class EmployeeWriter {
 		if (output != null) {
 			output.close();
 		}
+	}
+
+	private static ProjectDetails getProjectDetails() {
+		return ProjectDetails.newBuilder()
+					.setProjectName("XXXX")
+					.setProjectCode(00067)
+					.build();
+	}
+
+	private static Map<String, Integer> getAssessmentScores() {
+		Map<String, Integer> assessmentScores = new HashMap<>();
+		assessmentScores.put("AWS", 75);
+		assessmentScores.put("AZURE", 86);
+		assessmentScores.put("GCP", 94);
+		return assessmentScores;
+	}
+
+	private static ArrayList<String> getSkills() {
+		ArrayList<String> skills = new ArrayList<String>();
+		skills.add("Java");
+		skills.add("Python");
+		return skills;
 	}
 }
